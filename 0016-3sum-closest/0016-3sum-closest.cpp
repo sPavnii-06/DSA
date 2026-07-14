9cb96be098693313e4a3863345503ec8f799248c
Pavni Singh <singhpavni2006@gmail.com>
2026-07-14 10:42:04 +0530

    Time: 10 ms (65.07%), Space: 13.9 MB (78.97%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        // Sort the array to use the two-pointer technique
         sort(nums.begin(), nums.end());
         int n = nums.size();
         int closestSum = nums[0] + nums[1] + nums[2];
         
         for (int i = 0; i < n - 2; ++i) {
-            // Optimization: Skip duplicate values for the first element
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             int left = i + 1;
@@ -16,17 +15,15 @@ public:
             while (left < right) {
                 int currentSum = nums[i] + nums[left] + nums[right];
                 
-                // If we find an exact match, return immediately
+                
                 if (currentSum == target) {
                     return currentSum;
                 }
                 
-                // Update closest sum if the current one is closer to the target
+                
                 if (abs(currentSum - target) < abs(closestSum - target)) {
                     closestSum = currentSum;
                 }
-                
-                // Move pointers based on the comparison with target
                 if (currentSum < target) {
                     left++;
                 } else {
