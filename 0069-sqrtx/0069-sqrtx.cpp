d7f6457b789342c927a0922f0d19a8b1f62a942b
Pavni Singh <singhpavni2006@gmail.com>
2026-07-27 20:07:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2) return x;
+        int left=1; int right=x/2;
+
+        while (left<=right){
+        long long mid =left+(right -left)/2;
+        long long square = mid* mid;
+        if(square==x) return mid;
+        if(square<x) left=mid+1;
+        else right=mid-1;
+        
+        }
+        return right;
+    }
+};
